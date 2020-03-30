2ec90cb20d1170157f49fe7ae94a696482bbed66
gaiz <stan_gai@hotmail.com>
2020-03-30 21:20:08 +1100

    grpc server

---
+++ cmd/config.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	AppConfig AppConfig `yaml:"app" validate:"required"`
+}
+
+type AppConfig struct {
+	Port int `yaml:"port" validate:"required"`
+}
+
+var defaultConfig = &Config{AppConfig: AppConfig{Port: 8080}}
+
+func loadConfig(configFile string) (*Config, error) {
+	if configFile == "" {
+		return defaultConfig, nil
+	}
+	filePath := filepath.Clean(configFile)
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	var config *Config
+	if err := yaml.UnmarshalStrict(bytes, config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
